e2a63b497a95995cce8aeec1c6442b11fdf9d411
Deepakp404 <deepakp404@gmail.com>
2016-05-03 12:16:59 +0530

    Two examples to show what data which cant be assigned to int data type

---
+++ Session1_Assignment3.java
@@ -0,0 +1,13 @@
+
+public class Session1_Assignment3 
+{
+	public static void main(String[] args) 
+	{
+		int A;
+		// We can't assign character value to int data type
+		A = "a"; 
+		// We can't assign decimal value to int data type
+		A = 5.4;
+	}
+
+}
